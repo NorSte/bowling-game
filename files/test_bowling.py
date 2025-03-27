fe240d7fc1c78ec54d86141e86076bb83f82c262
3L5hxt <3L5hxt@cyber-dojo.org>
2025-03-27 13:54:51 +0000

    25 ran tests, predicted green, got red

--- files/test_bowling.py
+++ files/test_bowling.py
@@ -6,8 +6,15 @@ def test_all_strikes():
 def test_strikes_and_numbers():
     assert bowling("X|X|X|X|X|X|X|X|X|X||X6") == 296
     
-def test_strikes_with_spares():
+def test_strikes_and_spares():
     assert bowling("X|X|X|X|X|X|X|X|X|X||4/") == 284
     
 def test_miss_and_spare():
     assert bowling("X|X|-/|X|X|X|X|X|X|X||4/") == 254
+    
+def test_final_combination():
+    assert bowling("X|7/|9-|X|-8|8/|-6|X|X|X||81") == 164
+
+    
+    
+    
