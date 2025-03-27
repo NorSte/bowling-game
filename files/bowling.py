ba6c904aef09fabfd4a49190caf38d43a002fce6
3L5hxt <3L5hxt@cyber-dojo.org>
2025-03-27 12:29:04 +0000

    3 ran tests, predicted green, got red

--- files/bowling.py
+++ files/bowling.py
@@ -1,7 +1,21 @@
 # Example string format: X|X|X|X|X|X|X|X|X|X||XX
 
 def bowling(string):
-    if (string == "X|X|X|X|X|X|X|X|X|X||XX"):
-        return 300
+    clean_string = string.replace('|', '')
+    score = 0
+
+    # Should read all numbers and compute the final result
+    for i in range(10):
+        if clean_string[i] == "X":
+            score += compute_X(clean_string, i)
+        
+def compute_X(clean_string, i):
+    score = 10
     
+    if (clean_string[i+1] == "X"):
+        score += 10
+    if (clean_string[i+2] == "X"):
+        score += 10
+    
+    return score
     
